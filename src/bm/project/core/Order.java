82b868a12203adde3844c5f8eca3a50342f6ebef
psyy1204 <psyy1204@gmail.com>
2021-11-01 07:33:48 +0900

    Update Order.java

--- src/bm/project/core/Order.java
+++ src/bm/project/core/Order.java
@@ -11,20 +11,19 @@ public class Order {
     public List<OrderMenu> OrderMenus;
     // 표시형식을 자유롭게 하기 위해
     // 기본자료형으로 변경
-    public LocalDateTime OrderDate;
+    public String OrderDate;
 
     // 생성자에서 현재 시간을 가져오도록 수정
     // LocalDateTime now = LocalDateTime.now();
     // String orderDate = now.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초"));
 
-    public Order(){
-        OrderMenus = new ArrayList<>();
-    }
+    public Order(){ OrderMenus =  new ArrayList<>(); }
 
     public Order(List<OrderMenu> orderMenus) {
         this.OrderNo = Restaurant.orderNo++;
         this.OrderMenus = orderMenus;
-        this.OrderDate = LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.now();
+        this.OrderDate = now.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초"));
     }
 
     @Override
@@ -32,7 +31,7 @@ public class Order {
         return "Order{" +
                 "OrderMenus=" + OrderMenus.toString() +
                 // String 형태가 필요할 때 형식에 맞춰 출력되도록 수정
-                ", OrderDate='" + OrderDate.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초")) + '\'' +
+                ", OrderDate='" + OrderDate + '\'' +
                 '}';
     }
 }
